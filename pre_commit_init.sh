ccc2fa4397fcfe162f88ce58a610345e0889585f
Yu Leng <lengyuchn@gmail.com>
2023-04-09 01:45:22 +0800

    update

---
+++ pre_commit_init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install pre commit 
+echo "Install pre-commit"
+pip3 install pre-commit
+
+# Install golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
+
+# Install pre commit to .git
+pre-commit install
+
+# Run against all the files
+pre-commit run --all-files
